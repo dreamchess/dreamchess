6777a0f66ee94cb8cb5cfc62486029961da14aa7
Walter van Niftrik <walter@vanniftrik-it.nl>
2009-02-05 17:54:55 +0000

    * Added Lua class implementation from Lua wiki.

--- data/scripts/config_manager.lua
+++ data/scripts/config_manager.lua
@@ -1,3 +1,15 @@
+require 'class'
+
+ConfigManager = class(
+	function(c)
+		c.options = {}
+	end
+)
+
+function ConfigManager:AddOption(name, values)
+	self.options[name] = values
+end
+
 xml = engine.XMLReader()
 xml:LoadXMLFile("options.xml")
 while true
@@ -6,4 +18,8 @@ do
 	if option.name == nil then break end
 	print("Option", option.name, ":", option.value)
 end
+
+cm = ConfigManager()
+cm:AddOption("sound volume", {1, 2, 3})
+for i, v in pairs(cm.options) do print(i, v) end
 
--- src/libs/engine/test.cpp
+++ src/libs/engine/test.cpp
@@ -1,8 +1,10 @@
 #include "options.h"
+#include "string.h"
 #include <stdlib.h>
 #include <iostream>
 
 extern "C" {
+#include "debug.h"
 #include "dir.h"
 #include "lua.h"
 #include "lualib.h"
@@ -10,17 +12,59 @@ extern "C" {
 int luaopen_engine(lua_State* L);
 }
 
+static void loadfile(lua_State *L, char *filename)
+{
+	ch_datadir();
+	chdir("scripts");
+
+	if (luaL_loadfile(L, filename) != 0) {
+		DBG_ERROR("%s\n", lua_tostring (L, -1));
+		exit(1);
+	}
+
+	ch_userdir();
+
+	if (lua_pcall(L, 0, 0, 0) != 0) {
+		DBG_ERROR("%s\n", lua_tostring (L, -1));
+		exit(1);
+	}
+}
+
+static void setclasspath(lua_State *L)
+{
+	char path[PATH_MAX + 14 + 7] = "package.path='";
+
+	ch_datadir();
+	chdir("scripts");
+
+	if (!getcwd(path + 14, PATH_MAX)) {
+		DBG_ERROR("Error setting lua class path\n");
+		exit(1);
+	}
+
+	strcat(path, "/?.lua'");
+
+	if (luaL_loadstring(L, path) != 0) {
+		DBG_ERROR("%s\n", lua_tostring (L, -1));
+		exit(1);
+	}
+
+	if (lua_pcall(L, 0, 0, 0) != 0) {
+		DBG_ERROR("%s\n", lua_tostring (L, -1));
+		exit(1);
+	}
+}
+
 void testengine()
 {
-    lua_State *L = lua_open();
+	lua_State *L = lua_open();
 
-    luaL_openlibs(L);
-    luaopen_engine(L);
+	luaL_openlibs(L);
+	setclasspath(L);
+	luaopen_engine(L);
 
-    ch_datadir();
-    chdir("scripts");
-    luaL_loadfile(L, "config_manager.lua");
-    ch_userdir();
-    lua_pcall(L, 0, 0, 0);
+	loadfile(L, "config_manager.lua");
+
+	exit(0);
 }
 
