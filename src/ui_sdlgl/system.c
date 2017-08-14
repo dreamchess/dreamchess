679c986dd2aaa4279122eec1f6f94c9d9730bc08
Walter van Niftrik <walter@vanniftrik-it.nl>
2017-08-14 14:20:08 +0200

    Remove old glReadPixels depth buffer test
    We use colour picking now

--- src/ui_sdlgl/system.c
+++ src/ui_sdlgl/system.c
@@ -129,9 +129,6 @@ void init_gl(void)
     glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
 
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-    glReadPixels(100, 100, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &zerodepth);
-    if ( zerodepth != 1.0f )
-        DBG_WARN( "z depth should be 1.0f, but we got %f", zerodepth );
 
     glEnable(GL_BLEND);
 
