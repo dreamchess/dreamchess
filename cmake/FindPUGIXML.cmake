3bbc9cde2bcc3159e48f278200d8f384e973e906
Walter van Niftrik <walter@vanniftrik-it.nl>
2018-05-11 13:49:04 +0200

    Add pugixml 1.8 to search path

--- cmake/FindPUGIXML.cmake
+++ cmake/FindPUGIXML.cmake
@@ -1,5 +1,5 @@
-find_path(PUGIXML_INCLUDE_DIR NAMES pugixml.hpp PATH_SUFFIXES pugixml-1.9)
-find_library(PUGIXML_LIBRARY NAMES pugixml PATH_SUFFIXES pugixml-1.9)
+find_path(PUGIXML_INCLUDE_DIR NAMES pugixml.hpp PATH_SUFFIXES pugixml-1.8 pugixml-1.9)
+find_library(PUGIXML_LIBRARY NAMES pugixml PATH_SUFFIXES pugixml-1.8 pugixml-1.9)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PUGIXML DEFAULT_MSG PUGIXML_LIBRARY PUGIXML_INCLUDE_DIR)
