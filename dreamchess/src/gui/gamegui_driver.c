dreamchess/dreamchess

commit ccf5dda4828781add9cef321a48897d245df2254

Add Unicode support

dreamchess/src/gui/gamegui_driver.c
@@ -19,6 +19,7 @@
 */
 
 #include "ui_sdlgl.h"
+#include "unicode.h"
 
 static unsigned int utf8_to_utf32(const char *utf8) {
 	unsigned int char_utf32 = 0;
@@ -73,10 +74,7 @@ gg_event_t convert_event(SDL_Event *event) {
 			gg_event.key = GG_KEY_ESCAPE;
 			break;
 		default:
-			if (event->key.keysym.sym >= 32 || event->key.keysym.sym < 255)
-				gg_event.key = event->key.keysym.sym;
-			else
-				gg_event.type = GG_EVENT_NONE;
+			gg_event.type = GG_EVENT_NONE;
 			return gg_event;
 		}
 		break;
@@ -84,7 +82,8 @@ gg_event_t convert_event(SDL_Event *event) {
 	case SDL_TEXTINPUT: {
 		unsigned int utf32 = utf8_to_utf32(event->text.text);
 
-		if (utf32 > 0 && utf32 <= 0xff) {
+		// Accept ASCII input only
+		if (utf32 >= 0x20 && utf32 < 0x80) {
 			gg_event.type = GG_EVENT_CHAR;
 			gg_event.key = utf32 & 0xff;
 			return gg_event;
@@ -154,16 +153,17 @@ static void draw_image(void *image, gg_rect_t source, gg_rect_t dest, int mode_h
 					ysrc + height, en_h, en_v);
 }
 
-static void *get_char_image(int c) {
-	if (c < 0)
-		c += 256;
-
-	return get_text_character(c);
-	/*    return &text_characters[c];*/
+static unsigned int get_line_height(void) {
+	return (unsigned int)unicode_get_font_height();
 }
 
-static void draw_char(int c, int x, int y, gg_colour_t *colour) {
-	text_draw_char(x, y, 1.0f, c, colour);
+static unsigned int get_string_width(const char *text) {
+	return (unsigned int)unicode_get_string_width(text);
+}
+
+static void draw_string(const char *text, int x, int y, float align, int bounce, int no_shadow, gg_colour_t colour) {
+	const unsigned int flags = (bounce ? UNICODE_FLAG_BOUNCY : 0) | (no_shadow ? UNICODE_FLAG_NO_SHADOW : 0);
+	unicode_string_render(text, x, y, align, 1.0f, flags, colour);
 }
 
 static void get_image_size(void *image, int *width, int *height) {
@@ -176,17 +176,6 @@ static void get_image_size(void *image, int *width, int *height) {
 		*height = texture->height;
 }
 
-static void get_char_size(int c, int *width, int *height) {
-	if (c < 0)
-		c += 256;
-
-	if (width)
-		*width = get_text_character(c)->width;
-
-	if (height)
-		*height = get_text_character(c)->height;
-}
-
 static unsigned int get_ticks(void) {
 	return SDL_GetTicks();
 }
@@ -195,8 +184,9 @@ static float get_gl_screen_width(void) {
 	return get_gl_width();
 }
 
-gg_driver_t gg_driver_sdlgl = {draw_rect, draw_rect_fill, draw_rect_fill_gradient, draw_image, get_char_image,
-							   draw_char, get_image_size, get_char_size,		   get_ticks,  get_gl_screen_width};
+gg_driver_t gg_driver_sdlgl = {draw_rect, draw_rect_fill, draw_rect_fill_gradient, draw_image,
+							   get_image_size, get_ticks, get_gl_screen_width,
+							   get_line_height, get_string_width, draw_string};
 
 gg_driver_t *get_gg_driver_sdlgl(void) {
 	return &gg_driver_sdlgl;

.github/workflows/ci.yml
@@ -23,7 +23,7 @@ jobs:
     - name: Install Dependencies
       run: |
         sudo apt-get update
-        sudo apt-get -y install gettext libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libglew-dev
+        sudo apt-get -y install gettext libfreetype-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libglew-dev
 
     - uses: actions/checkout@v3
 
@@ -50,7 +50,7 @@ jobs:
       with:
         msystem: ${{ matrix.sys }}
         install: base-devel
-        pacboy: toolchain:p cmake:p SDL2:p SDL2_image:p SDL2_mixer:p expat:p glew:p
+        pacboy: toolchain:p cmake:p gettext:p freetype:p SDL2:p SDL2_image:p SDL2_mixer:p expat:p glew:p
 
     - uses: actions/checkout@v3
 

.gitignore
@@ -1,2 +1,2 @@
-/build/
+/build*/
 .DS_Store

dreamchess/data/font.wid
@@ -1,1 +1,0 @@
-000000(.%((((0!!"%%$!)$&&!#!/!!(/'(0(0(///!!!!!!/"%$&&&&&(&####/(

dreamchess/src/CMakeLists.txt
@@ -7,7 +7,14 @@ unset(CMAKE_FIND_PACKAGE_PREFER_CONFIG)
 
 find_package(OpenGL REQUIRED)
 find_package(GLEW REQUIRED)
+find_package(Freetype REQUIRED)
 find_package(EXPAT REQUIRED)
+
+include(CheckLibraryExists)
+check_library_exists(m cos "" HAVE_LIBM)
+if(HAVE_LIBM)
+    set(MATH_LIBRARY "m")
+endif()
 
 BISON_TARGET(PgnParser pgn_parser.y ${CMAKE_CURRENT_BINARY_DIR}/pgn_parser.c)
 FLEX_TARGET(PgnScanner pgn_scanner.l ${CMAKE_CURRENT_BINARY_DIR}/pgn_scanner.c)
@@ -169,7 +176,6 @@ target_sources(dreamchess PRIVATE
     gui/promote_dialog.c
     gui/saveload_dialog.c
     gui/system.c
-    gui/text.c
     gui/texture.c
     gui/theme.c
     gui/title_dialog.c
@@ -178,9 +184,27 @@ target_sources(dreamchess PRIVATE
     gui/ui_sdlgl_3d.h
     gui/ui_sdlgl.c
     gui/ui_sdlgl.h
+    gui/unicode.c
+    gui/unicode.h
     gui/victory_dialog.c
-    gui/vkeyboard.c
     gui/xmlsavefile.c
+    gui/freetype-gl/distance-field.c
+    gui/freetype-gl/distance-field.h
+    gui/freetype-gl/edtaa3func.c
+    gui/freetype-gl/edtaa3func.h
+    gui/freetype-gl/freetype-gl-errdef.h
+    gui/freetype-gl/ftgl-utils.c
+    gui/freetype-gl/ftgl-utils.h
+    gui/freetype-gl/platform.c
+    gui/freetype-gl/platform.h
+    gui/freetype-gl/texture-atlas.c
+    gui/freetype-gl/texture-atlas.h
+    gui/freetype-gl/texture-font.c
+    gui/freetype-gl/texture-font.h
+    gui/freetype-gl/utf8-utils.c
+    gui/freetype-gl/utf8-utils.h
+    gui/freetype-gl/vector.c
+    gui/freetype-gl/vector.h
 )
 
 source_group("Source Files\\gui" REGULAR_EXPRESSION /gui/.*.c$)
@@ -195,6 +219,7 @@ target_include_directories(dreamchess PRIVATE
     ${SDL2_MIXER_INCLUDE_DIR}
     ${OPENGL_INCLUDE_DIR}
     ${GLEW_INCLUDE_DIR}
+    ${FREETYPE_INCLUDE_DIRS}
     ${EXPAT_INCLUDE_DIR}
     ${Intl_INCLUDE_DIRS}
     ${CMAKE_CURRENT_SOURCE_DIR}
@@ -202,9 +227,12 @@ target_include_directories(dreamchess PRIVATE
     ${CMAKE_BINARY_DIR}
 )
 
+set_property(TARGET dreamchess PROPERTY C_STANDARD 99)
+
 target_link_libraries(dreamchess PRIVATE
     common
     GLEW::GLEW
+    Freetype::Freetype
     $<IF:$<TARGET_EXISTS:SDL2_image::SDL2_image>,SDL2_image::SDL2_image,$<TARGET_NAME_IF_EXISTS:SDL2_image::SDL2_image-static>>
     ${SDL2_IMAGE_LIBRARY}
     $<IF:$<TARGET_EXISTS:SDL2_mixer::SDL2_mixer>,SDL2_mixer::SDL2_mixer,$<TARGET_NAME_IF_EXISTS:SDL2_mixer::SDL2_mixer-static>>
@@ -214,6 +242,7 @@ target_link_libraries(dreamchess PRIVATE
     ${OPENGL_LIBRARY}
     EXPAT::EXPAT
     ${Intl_LIBRARIES}
+    ${MATH_LIBRARY}
 )
 
 if(WIN32)

commit ed1b1354099282974edea3281d2089592846f428

Restore keyboard shortcut key functionality
This was a regression from ccf5dda

dreamchess/src/gui/gamegui_driver.c
@@ -74,7 +74,10 @@ gg_event_t convert_event(SDL_Event *event) {
 			gg_event.key = GG_KEY_ESCAPE;
 			break;
 		default:
-			gg_event.type = GG_EVENT_NONE;
+			if (event->key.keysym.sym >= 0x20 && event->key.keysym.sym < 0x80)
+				gg_event.key = event->key.keysym.sym;
+			else
+				gg_event.type = GG_EVENT_NONE;
 			return gg_event;
 		}
 		break;